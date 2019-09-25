b88b230100107538a91895b8e2782c3f2108d627
chxy95 <824534882@qq.com>
2019-09-26 01:34:12 +0800

    extract rects from images.

--- extract_rect.py
+++ extract_rect.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Sep 25 19:01:47 2019
+Created on Thu Jul 11 21:44:47 2019
 
 @author: chxy + zwzhang
 """
 
 import cv2
-import time
 import os.path as osp
 
 ###########################################################################################
@@ -37,34 +36,42 @@ model_name.insert(0, 'HR')
 
 def on_mouse(event, x, y, flags, param):
     
-	global point1, point2
-	img = imgs[0].copy()
-
-	if event == cv2.EVENT_LBUTTONDOWN:    # 左键点击
-		point1 = (x,y)    # 起点
-		cv2.circle(img, point1, 10, (0,255,0), 5)
-		cv2.imshow('image', img)
-	elif event == cv2.EVENT_MOUSEMOVE and (flags & cv2.EVENT_FLAG_LBUTTON):    # 按住左键拖曳
-		cv2.rectangle(img, point1, (x, y), (255,0,0), 5)
-		cv2.imshow('image', img)
-	elif event == cv2.EVENT_LBUTTONUP:    # 左键释放
-		point2 = (x,y)    # 终点
-		cv2.rectangle(img, point1, point2, (0,0,255), 3)
-		cv2.imshow('image', img)
-		for i in range(len(imgs)):
-			img_result = imgs[i].copy()
-
-			min_x = min(point1[0], point2[0])     
-			min_y = min(point1[1], point2[1])
-			width = abs(point1[0] - point2[0])
-			height = abs(point1[1] -point2[1])
-			
-			rect = img_result[min_y:min_y+height, min_x:min_x+width]
-			cv2.imwrite(osp.join(save_path, model_name[i] + '_rect.png'), rect)
-			
-			cv2.rectangle(img_result, point1, point2, (0,0,255), 3) 
-			cv2.imwrite(osp.join(save_path, model_name[i] + '_with_rect.png'), img_result)
-		
+    global point1, point2
+    img = imgs[0].copy()
+    
+    if event == cv2.EVENT_LBUTTONDOWN:    # 左键点击
+        point1 = (x,y)    # 起点
+        cv2.circle(img, point1, 10, (0,255,0), 5)
+        cv2.imshow('image', img)
+    elif event == cv2.EVENT_MOUSEMOVE and (flags & cv2.EVENT_FLAG_LBUTTON):    # 按住左键拖曳
+        cv2.rectangle(img, point1, (x, y), (255,0,0), 5)
+        cv2.imshow('image', img)
+    elif event == cv2.EVENT_LBUTTONUP:    # 左键释放
+        point2 = (x,y)    # 终点
+        cv2.rectangle(img, point1, point2, (0,0,255), 3)
+        cv2.imshow('image', img)
+        for i in range(len(imgs)):
+            img_result = imgs[i].copy()
+            
+            min_x = min(point1[0], point2[0])     
+            min_y = min(point1[1], point2[1])
+            width = abs(point1[0] - point2[0])
+            height = abs(point1[1] -point2[1])
+            
+            # 将矩形截取的内容输出
+            rect = img_result[min_y:min_y+height+1, min_x:min_x+width+1]
+            cv2.imwrite(osp.join(save_path, model_name[i] + '_rect.png'), rect)
+            
+            # 将图片与矩形框同时输出
+            cv2.rectangle(img_result, point1, point2, (0,0,255), 2) 
+            cv2.imwrite(osp.join(save_path, model_name[i] + '_with_rect.png'), img_result)
+            
+            # 将矩形截取的特征放大scale倍置于图片右下角输出，未考虑重叠情况
+            scale = 3
+            img_result[img_result.shape[0] - scale * (1+height) : img_result.shape[0], 
+            img_result.shape[1] - scale * (1+width) : img_result.shape[1]] = cv2.resize(rect, (0, 0), fx=scale, fy=scale)
+            cv2.imwrite(osp.join(save_path, model_name[i] + '_with_fea.png'), img_result)
+        		
 def main():
 
 	cv2.namedWindow('image')
