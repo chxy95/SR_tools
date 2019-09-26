efc36e35ff22e5c439043c7363391219cb8743aa
chxy95 <824534882@qq.com>
2019-09-26 12:42:12 +0800

    extract rects from images.

--- extract_rect.py
+++ extract_rect.py
@@ -15,13 +15,13 @@ save_path = './asserts'
 read_path = './testset'
 
 # utilized models name 
-model_name = ['MSRResNetx4_res16', 'MSRGANx4_res1_pw0', 'MSRGANx4_res4', 'MSRGANx4_res8', 'MSRGANx4_res16', 'RRDB_ESRGAN_x4']
+model_name = ['Bicubic_x2', 'SRCNN_x2', 'FSRCNN_x2', 'RCAN_x2']
 num_model = len(model_name)
 
 # tested img
 test_img = 'HR_test.png'
 
-# The HR image of model 'MSRResNetx4_res16' is named 'MSRResNetx4_res16_test.png'.
+# The HR image of model 'SRCNN_x2' is named 'SRCNN_x2_test.png'.
 
 ###########################################################################################
 
@@ -38,13 +38,14 @@ def on_mouse(event, x, y, flags, param):
     
     global point1, point2
     img = imgs[0].copy()
+    cv2.imshow('image', img)
     
     if event == cv2.EVENT_LBUTTONDOWN:    # 左键点击
         point1 = (x,y)    # 起点
         cv2.circle(img, point1, 10, (0,255,0), 5)
         cv2.imshow('image', img)
     elif event == cv2.EVENT_MOUSEMOVE and (flags & cv2.EVENT_FLAG_LBUTTON):    # 按住左键拖曳
-        cv2.rectangle(img, point1, (x, y), (255,0,0), 5)
+        cv2.rectangle(img, point1, (x, y), (0,255,255), 5)
         cv2.imshow('image', img)
     elif event == cv2.EVENT_LBUTTONUP:    # 左键释放
         point2 = (x,y)    # 终点
@@ -59,14 +60,16 @@ def on_mouse(event, x, y, flags, param):
             height = abs(point1[1] -point2[1])
             
             # 将矩形截取的内容输出
-            rect = img_result[min_y:min_y+height+1, min_x:min_x+width+1]
+            rect = img_result[min_y:min_y+height+1, min_x:min_x+width+1]    # 可修改截取参数保存固定尺寸或正方形截取
             cv2.imwrite(osp.join(save_path, model_name[i] + '_rect.png'), rect)
+            cv2.imshow('fea', rect)
             
             # 将图片与矩形框同时输出
-            cv2.rectangle(img_result, point1, point2, (0,0,255), 2) 
+            cv2.rectangle(img_result, point1, point2, (0,255,255), 2) 
             cv2.imwrite(osp.join(save_path, model_name[i] + '_with_rect.png'), img_result)
+            cv2.imshow('fea_with_rect', rect)
             
-            # 将矩形截取的特征放大scale倍置于图片右下角输出，未考虑重叠情况
+            # 将矩形截取的特征进行resize后置于图片右下角输出，未考虑重叠情况
             scale = 3
             img_result[img_result.shape[0] - scale * (1+height) : img_result.shape[0], 
             img_result.shape[1] - scale * (1+width) : img_result.shape[1]] = cv2.resize(rect, (0, 0), fx=scale, fy=scale)
@@ -76,7 +79,6 @@ def main():
 
 	cv2.namedWindow('image')
 	cv2.setMouseCallback('image', on_mouse)
-	cv2.imshow('image', imgs[0])
 	cv2.waitKey(0)
 
 if __name__ == '__main__':
